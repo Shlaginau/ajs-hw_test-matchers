e1ff853b32445ccc2cfafb8756dc4d4fd6c1360f
Uliana Shlagina <shlaginau@gmail.com>
2023-08-28 00:15:15 +0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import sortHeroesByHealth from './sortHeroesByHealth';
+
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+sortHeroesByHealth(heroes);
